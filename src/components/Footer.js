fd7c8bdaa6109a9243451e05e79bbba97dedf3d6
Nasty G <skalakul@gmail.com>
2020-12-09 11:54:54 +0100

    landingPage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,7 @@ const Footer = () => {
         <footer className = "footer">
              <div>&copy; {new Date().getFullYear()} | Dodewaards Mannenkoor</div> 
                 <a href="https://www.linkedin.com/in/sawitree-kalakul/" 
-                 target="_blank" rel="noreferrer" className="footer__link">
-                By Sawitree Kalakul </a>
+                 target="_blank" rel="noreferrer" className="footer__link">By Sawitree Kalakul </a>
              </footer> 
     );
 }
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import Slider from 'infinite-react-carousel';
+import  image0  from '../assets/cover.jpg'
+import  image1  from '../assets/intro-1.jpg'
 
 const LandingPage = () =>{
 
     return(
         <div className ="landingPage__container">
-            <div className ="landingPage__updateCard"/>
         </div>
     )
 }
 
 export default LandingPage;
+//<img className ="landingPage__slide__item" src ={image0} alt= "A choir group"/>
